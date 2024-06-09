54b505b8de055345586edf994f9917be954682ee
sara abdelsalam <105568705+saraabdelsalam@users.noreply.github.com>
2024-06-09 17:47:21 +0300

    singleton implementation

--- Creational Patterns/Singleton/Counter.cs
+++ Creational Patterns/Singleton/Counter.cs
@@ -4,6 +4,36 @@ namespace Creational_Patterns.Singleton
 {
     public class Counter
     {
+        private static Counter instance;
+        private static readonly object lockObject = new object();
+        private int count = 0;
 
+        private Counter() { }
+
+        public static Counter GetInstance()
+        {
+            // Double-checked locking to ensure thread safety
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Counter();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public int Count => count;
+
+        public void IncreaseCount()
+        {
+            lock (lockObject)
+            {
+                count++;
+            }
+        }
     }
 }
--- Design Patterns/Program.cs
+++ Design Patterns/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Design_Patterns
+﻿using Creational_Patterns.Singleton;
+
+namespace Design_Patterns
 {
     public class Program
     {
         static void Main(string[] args)
         {
             #region Creational Patterns
+            #region singleton
+            Task task1 = Task.Factory.StartNew(() =>
+            {
+                Counter counter1 = Counter.GetInstance();
+                counter1.IncreaseCount();
+                Console.WriteLine("counter 1 :" + counter1.Count.ToString());
+            });
+
+            Task task2 = Task.Factory.StartNew(() =>
+            {
+                Counter counter2 = Counter.GetInstance();
+                counter2.IncreaseCount();
+                Console.WriteLine("counter 2 :" + counter2.Count.ToString());
+             
+            });
+            Task.WaitAll(task1, task2); // Wait for both tasks to complete before exiting
+
+            Console.WriteLine("Tasks completed.");
+            #endregion
             #endregion
 
             #region Structural Patterns
